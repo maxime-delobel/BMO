fd44e1805a7169a43a8478a9c0a229cfce4ef01a
Maxime Delobel <maxime.delobel@student.hogent.be>
2026-02-10 16:50:50 +0100

    added setup script + readme

---
+++ Setup.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "This script will set the necessary environment variables for BMO to work"
+Write-Host "First, make an API key on Google AI Studio, see README.MD for instructions"
+$api_key = Read-Host -Prompt "Paste the API key:"
+[System.Environment]::SetEnvironmentVariable("GEMINI_API_KEY", "$api_key", "User")
+Write-Host "All done! Restart IDE (VsCode) for changes to take effect"
